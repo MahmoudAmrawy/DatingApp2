b44544f6c0f18502cf33fc62fd5321060f1c989c
mahmoudElamrawy <mahmoudamrawy9@gmail.com>
2022-06-04 23:48:43 +0200

    end to section 3

--- API/Startup.cs
+++ API/Startup.cs
@@ -31,7 +31,20 @@ namespace API
             });
 
             services.AddControllers();
-        }
+            services.AddCors(options => {
+
+            options.AddPolicy(
+
+            name: "AllowOrigin",
+
+            builder => {
+
+            builder.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader();
+
+            });
+
+        });
+    }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
@@ -44,6 +57,8 @@ namespace API
             app.UseHttpsRedirection();
 
             app.UseRouting();
+
+            app.UseCors("AllowOrigin");
 
             app.UseAuthorization();
 
